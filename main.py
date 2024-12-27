cb9aa23e50319eb31e71f201148a79b196827e82
User <dawidsuan@yandex.ru>
2024-12-27 05:15:49 +0300

    Добавлена обработка карт и сбор светлячков

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 import sys
+from typing import Optional
+from random import choice, randint
+
 import pygame
 from PIL import ImageFilter, Image
 
@@ -8,7 +12,7 @@ settings = {
 }
 
 data = {
-    'level': 0,
+    'level': 1,
     'km': 0,
     'fireflies': 0,
     'stars': 0
@@ -315,44 +319,224 @@ def start_menu(screen: pygame.Surface, clock: pygame.time.Clock, cursor: Cursor)
         clock.tick(settings['fps'])
 
 
+def load_image(name, colorkey=None, scale:Optional[tuple[int, int]]=None, flip:Optional[tuple[bool, bool]]=None) -> pygame.Surface:
+    fullname = os.path.join('resource/image', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if scale is not None:
+        image = pygame.transform.scale(image, scale)
+    if flip is not None:
+        image = pygame.transform.flip(image, *flip)
+
+    return image
+
+
+def load_level(filename):
+    filename = "resource/level/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.') + '#', level_map))
+
+
+player = None
+tile_images: dict[str, pygame.Surface] = {
+    'dirty': load_image('dirty.png'),
+    'dirty2': load_image('dirty2.png'),
+    'dirty3': load_image('dirty3.png'),
+    'wall': load_image('cobblestone.png'),
+    'grass': load_image('grass.png'),
+    'grass2': load_image('grass2.png'),
+    'tree': load_image('tree.png'),
+    'forest': load_image('forest_bg.png')
+}
+tile_width = tile_height = 64
+tiles_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+collide_sprites = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+class Firefly:
+    def __init__(self, x, y, surf):
+        self.x = x
+        self.y = y
+        self.vx = -5
+        self.vy = -5
+        self.surf = surf
+
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+        if self.y < 10 or self.y > tile_height - 10:
+            self.vy = -self.vy
+        if self.x < 10 or self.x > tile_width - 10:
+            self.vx = -self.vx
+
+
+    def draw(self):
+        pygame.draw.circle(self.surf, (255, 255, 255, 255), (self.x, self.y), 5)
+
+
+
+class Fireflies(pygame.sprite.Sprite):
+    def __init__(self, pos_x: int, pos_y: int):
+        super().__init__(tiles_group, all_sprites)
+        self.image = pygame.Surface((64, 64), pygame.SRCALPHA)
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        self.mask = pygame.mask.from_surface(pygame.Surface((64, 64)))
+        self.taken = False
+
+        self.quantity = randint(1, 5)
+        positions = [(randint(10, tile_width - 10), randint(10, tile_height - 10)) for _ in range(self.quantity)]
+        self.fireflies = list()
+        for pos in positions:
+            self.fireflies.append(Firefly(*pos, self.image))
+
+    def update(self):
+        self.image.fill((0, 0, 0, 0))
+        for firefly in self.fireflies:
+            firefly.move()
+            firefly.draw()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type: str, pos_x: int, pos_y: int):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x: int, pos_y: int):
+        super().__init__(player_group, all_sprites)
+        self.image = load_image('player.png', scale=(100, 150))
+        self.image_left = pygame.transform.flip(self.image, True, False)
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 15)
+        self.speed = 5
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def check_collide(self):
+        for sprite in collide_sprites:
+            if pygame.sprite.collide_mask(self, sprite):
+                if isinstance(sprite, Fireflies):
+                    sprite.taken = True
+                    sprite.image.fill((0, 0, 0, 0))
+                    data['fireflies'] += sprite.quantity
+                    sprite.quantity = 0
+                    collide_sprites.remove_internal(sprite)
+                    return False
+                return True
+        return False
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '@':
+                new_player = Player(x, y)
+                pass
+            elif level[y][x] == '*':
+                Fireflies(x, y)
+            elif level[y][x] in '|&':
+                tile_name = {
+                    '|': 'tree',
+                    '&': 'forest'
+                }[level[y][x]]
+                img = tile_images[tile_name]
+
+                Tile(tile_name, x + img.get_width() // tile_width, y - img.get_height() // tile_height + 1)
+            else:
+                symbol = level[y][x]
+                if symbol != '.':
+                    Tile({
+                        '_': choice(('grass', 'grass2')),
+                        '#': 'wall',
+                        '+': 'dirty3',
+                        '=': 'dirty2',
+                        '-': 'dirty'
+                    }[symbol], x, y)
+
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
+
+
 def game():
     running = True
 
-    fairy_img = pygame.image.load('resource/image/player.png')
-    fairy_img = pygame.transform.scale(fairy_img, (100, 150))
-    fairy_img_right = fairy_img
-    fairy_img_left = pygame.transform.flip(fairy_img, True, False)
-    fairy_img = fairy_img_right
+    player, cx, cy = generate_level(load_level(f"{data['level']}.txt"))
 
+    for sprite in all_sprites:
+        if sprite not in player_group and sprite.image not in (tile_images['tree'], tile_images['forest']):
+            collide_sprites.add(sprite)
 
-    fairy_x, fairy_y = 250, 125
-    fairy_speed = 5
+    camera = Camera()
 
+    player_img = player.image
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                terminate()
 
+        last_x, last_y = player.rect.x, player.rect.y
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            player.rect.y -= player.speed
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            player.rect.y += player.speed
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            player.rect.x -= player.speed
+            player_img = player.image
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            player.rect.x += player.speed
+            player_img = player.image_left
+        if keys[pygame.KMOD_SHIFT]:
+            print(True)
 
-
-        if keys[pygame.K_w]:
-            fairy_y -= fairy_speed
-        if keys[pygame.K_s]:
-            fairy_y += fairy_speed
-        if keys[pygame.K_a]:
-            fairy_x -= fairy_speed
-            fairy_img = fairy_img_right
-        if keys[pygame.K_d]:
-            fairy_x += fairy_speed
-            fairy_img = fairy_img_left
-
-
-        # fairy_x = max(0, min(fairy_x, 800 - fairy_img.get_width()))
-        # fairy_y = max(0, min(fairy_y, 600 - fairy_img.get_height()))
+        if player.check_collide():
+            player.rect.x = last_x
+            player.rect.y = last_y
 
         screen.fill(settings['screen_background'])
-        screen.blit(fairy_img, (fairy_x, fairy_y))
+
+        camera.update(player)
+        for sprite in all_sprites:
+            camera.apply(sprite)
+            if sprite not in player_group:
+                if isinstance(sprite, Fireflies) and not sprite.taken:
+                    sprite.update()
+                screen.blit(sprite.image, sprite.rect)
+
+        screen.blit(player_img, player.rect)
+        screen.blit(Label(f"Светлячков: {data['fireflies']}", 12).text, (10, 10))
+
         pygame.display.flip()
         clock.tick(settings['fps'])
 
@@ -366,7 +550,6 @@ if __name__ == '__main__':
     pygame.mouse.set_visible(False)
 
     clock = pygame.time.Clock()
-    screen.fill(settings['screen_background'])
 
     cursor = Cursor(screen)
 
